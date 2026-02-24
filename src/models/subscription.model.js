d49a7965b10f89d928235e8b0f8c3c71c8443d7b
priyansh-goel <priyanshgoel548@gmail.com>
2026-02-24 19:33:16 +0530

    subscription and user account management endpoint update

---
+++ src/models/subscription.model.js
@@ -0,0 +1,18 @@
+import mongooose, { Schema } from "mongoose";   
+
+const subscriptionSchema = new Schema({
+    subscriber:{
+        type: Schema.Types.ObjectId, //one who is subscribing 
+        ref: "User",
+        required: true
+    },
+    channel:{
+        type: Schema.Types.ObjectId, //one who is being subscribed to
+        ref: "User",
+        required: true
+    }
+},{
+    timestamps: true
+})
+
+export const Subscription = mongooose.model("Subscription", subscriptionSchema)
